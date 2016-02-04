18360cddc17450e235908e100f20114eab648e29
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2016-02-04 01:54:40 -0200

    Add config using Go.

--- src/main/java/org/gemignani/henrique/gocd/http_notifier/GoNotificationPlugin.java
+++ src/main/java/org/gemignani/henrique/gocd/http_notifier/GoNotificationPlugin.java
@@ -1,11 +1,16 @@
 package org.gemignani.henrique.gocd.http_notifier;
 
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.thoughtworks.go.plugin.api.GoApplicationAccessor;
 import com.thoughtworks.go.plugin.api.GoPlugin;
 import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
 import com.thoughtworks.go.plugin.api.annotation.Extension;
 import com.thoughtworks.go.plugin.api.logging.Logger;
+import com.thoughtworks.go.plugin.api.request.DefaultGoApiRequest;
+import com.thoughtworks.go.plugin.api.request.GoApiRequest;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 
@@ -17,6 +22,9 @@ public class GoNotificationPlugin
     private static Logger LOGGER = Logger.getLoggerFor(GoNotificationPlugin.class);
     public static final String EXTENSION_TYPE = "notification";
     private static final List<String> goSupportedVersions = Arrays.asList("1.0");
+    public static final String REQUEST_PLUGIN_SETTINGS_GET_CONFIGURATION = "go.plugin-settings.get-configuration";
+    public static final String REQUEST_PLUGIN_SETTINGS_GET_VIEW = "go.plugin-settings.get-view";
+    public static final String REQUEST_PLUGIN_SETTINGS_VALIDATE_CONFIGURATION = "go.plugin-settings.validate-configuration";
     public static final String REQUEST_NOTIFICATIONS_INTERESTED_IN = "notifications-interested-in";
     public static final String REQUEST_STAGE_STATUS = "stage-status";
     public static final int SUCCESS_RESPONSE_CODE = 200;
@@ -25,19 +33,92 @@ public class GoNotificationPlugin
 
     @Override
     public void initializeGoApplicationAccessor(GoApplicationAccessor goApplicationAccessor) {
-        PluginConfig pluginConfig = new PluginConfig();
+        DefaultGoApiRequest apiRequest = new DefaultGoApiRequest("go.processor.plugin-settings.get", "1.0", pluginIdentifier());
+        goApplicationAccessor.submit(apiRequest).responseBody();
+        PluginConfig pluginConfig = new Gson().fromJson(goApplicationAccessor.submit(apiRequest).responseBody(), PluginConfig.class);
+
         pipelineListener = new PipelineListener(pluginConfig);
     }
 
     @Override
     public GoPluginApiResponse handle(GoPluginApiRequest goPluginApiRequest) {
         LOGGER.debug("received go plugin api request " + goPluginApiRequest.requestName());
+        if (goPluginApiRequest.requestName().equals(REQUEST_PLUGIN_SETTINGS_GET_CONFIGURATION))
+            return handlePluginSettingsGetConfiguration();
+        if (goPluginApiRequest.requestName().equals(REQUEST_PLUGIN_SETTINGS_GET_VIEW))
+            return handlePluginSettingsGetView();
+        if (goPluginApiRequest.requestName().equals(REQUEST_PLUGIN_SETTINGS_VALIDATE_CONFIGURATION))
+            return handlePluginSettingsValidateConfiguration();
         if (goPluginApiRequest.requestName().equals(REQUEST_NOTIFICATIONS_INTERESTED_IN))
             return handleNotificationsInterestedIn();
         if (goPluginApiRequest.requestName().equals(REQUEST_STAGE_STATUS)) {
             return handleStageNotification(goPluginApiRequest);
         }
         return null;
+    }
+
+    private GoPluginApiResponse handlePluginSettingsGetConfiguration() {
+        return new GoPluginApiResponse() {
+            @Override
+            public int responseCode() {
+                return 0;
+            }
+
+            @Override
+            public Map<String, String> responseHeaders() {
+                return null;
+            }
+
+            @Override
+            public String responseBody() {
+                JsonObject object = new JsonObject();
+                JsonObject item = new JsonObject();
+                item.addProperty("display-name", "URL");
+                item.addProperty("display-order", "0");
+                object.add("url", item);
+                return object.toString();
+            }
+        };
+    }
+
+    private GoPluginApiResponse handlePluginSettingsGetView() {
+        return new GoPluginApiResponse() {
+            @Override
+            public int responseCode() {
+                return 0;
+            }
+
+            @Override
+            public Map<String, String> responseHeaders() {
+                return null;
+            }
+
+            @Override
+            public String responseBody() {
+                JsonObject object = new JsonObject();
+                object.addProperty("template", "<div class=\"form_item_block\"><label>URL:<span class=\"asterisk\">*</span><input type=\"text\" ng-model=\"url\" ng-required=\"true\"></label></div>");
+                return object.toString();
+            }
+        };
+    }
+
+    private GoPluginApiResponse handlePluginSettingsValidateConfiguration() {
+        return new GoPluginApiResponse() {
+            @Override
+            public int responseCode() {
+                return 0;
+            }
+
+            @Override
+            public Map<String, String> responseHeaders() {
+                return null;
+            }
+
+            @Override
+            public String responseBody() {
+                return new JsonArray().toString();
+            }
+        };
     }
 
     @Override
--- src/main/java/org/gemignani/henrique/gocd/http_notifier/PluginConfig.java
+++ src/main/java/org/gemignani/henrique/gocd/http_notifier/PluginConfig.java
@@ -1,6 +1,10 @@
 package org.gemignani.henrique.gocd.http_notifier;
 
+import com.thoughtworks.go.plugin.api.GoApplicationAccessor;
 import com.thoughtworks.go.plugin.api.logging.Logger;
+import com.thoughtworks.go.plugin.api.request.DefaultGoApiRequest;
+import com.thoughtworks.go.plugin.api.request.GoApiRequest;
+import com.thoughtworks.go.plugin.api.response.GoApiResponse;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
@@ -8,26 +12,10 @@ import java.io.File;
 
 public class PluginConfig {
     private static Logger LOGGER = Logger.getLoggerFor(GoNotificationPlugin.class);
-    private static final String PLUGIN_CONF = "gocd-websocket-notifier.conf";
-    private int port = 8887;
     private String url;
 
     public PluginConfig() {
-        String userHome = System.getProperty("user.home");
-        File pluginConfig = new File(userHome + File.separator + PLUGIN_CONF);
-        if (!pluginConfig.exists()) {
-            LOGGER.warn(String.format("Config file %s was not found in %s. Using default values.", PLUGIN_CONF, userHome));
-        }
-        else {
-            Config config = ConfigFactory.parseFile(pluginConfig);
-            if (config.hasPath("port")) {
-                setPort(config.getInt("port"));
-            }
-        }
     }
-
-    public int getPort() { return port; }
-    public void setPort(int port) { this.port = port; }
 
     public String getUrl() {
         return url;
